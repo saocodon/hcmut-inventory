d7557536efc65c4939108fe70cd4db5b8fb32ca7
matchatonari <khanhdii10a15@gmail.com>
2024-09-24 21:54:09 +0700

    DSA1 5đ

---
+++ CO2003/dsastudents/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include "list/listheader.h"
+#include "list/DLinkedListDemo.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    //cout << "Assignment-1" << endl;
+    dlistDemo1();
+    dlistDemo2();
+    dlistDemo3();
+    dlistDemo4();
+    dlistDemo5();
+    dlistDemo6();
+
+    return 0;
+}
+
